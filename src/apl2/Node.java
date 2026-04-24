b13d07211cf4624ffa403f22771a51b2d63c02a7
Kauã de Jesus Soares <kauaje18@gmail.com>
2026-04-23 22:21:41 -0300

    Novo node da Isabel

--- src/apl2/Node.java
+++ src/apl2/Node.java
@@ -1,21 +1,46 @@
-// arquivo: src/apl2/Node.java
-
-// TODO: Colocar a identificação dos(as) integrantes aqui.
-
-package apl2;
-
-// -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
-// representam a nova versão dos dados de uma pessoa, conforme descrito no
-// enunciado da atividade Apl2.
-// -- A classe deve conter os construtores apropriados, assim como os métodos
-// getters e setters.
-// -- A classe também representa um nó que é usado na implementação da lista
-// duplamente encadeada (classe DLinkedList).
-// -- A classe deve sobrescrever (override) o método public String toString()
-// {...}, retornando uma string com os valores dos atributos da classe.
-
 public class Node {
+
+	// dados da pessoa a serem armazenados no Node (nó)
+	private String id;
+	private String nome;
+	private float nota;
 	
-	// TODO: Implementar a classe conforme o enunciado da atividade Apl2.
+	// ponteiro para o próximo/anterior Node (nó) da lista ligada
+	private Node prox;
+	private Node ant;
 	
+	// construtor vazio
+	public Node() {}
+	
+	// construtor com parâmetros
+	public Node(String id, String nome, float nota) {
+		this.id = id;
+		this.nome = nome;
+		this.nota = nota;
+		this.prox = null;
+		this.ant = null;
+	}
+	
+	// getters (obtém) e setters (atribui) dos dados da pessoa
+	public String getId() { return id; }
+	public void setId(String id) { this.id = id; }
+	public String getNome() { return nome; }
+	public void setNome(String nome) { this.nome = nome; }
+	public float getNota() { return nota; }
+	public void setNota(float nota) { this.nota = nota; }
+	
+	// getProx(): obtém o ponteiro para o próximo Node (nó) da lista ligada
+	public Node getProx() { return prox; }
+	// setProx(): atribui o endereço do próximo Node (nó) da lista ligada
+	public void setProx(Node prox) { this.prox = prox; }
+	
+	// getAnt(): obtém o ponteiro para o Node (nó) anterior da lista ligada
+	public Node getAnt() { return ant; }
+	// setAnt(): atribui o endereço do Node (nó) anterior da lista ligada
+	public void setAnt(Node ant) { this.ant = ant; }
+	
+	@Override
+	public String toString() {
+		return id + ";" + nome + ";" + nota;
+	}
 }
